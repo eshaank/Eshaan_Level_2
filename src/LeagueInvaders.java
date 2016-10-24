0e0d195ee420e3cfe5c9430b7ece241b720a40fe
League <jiwenzou@gmail.com>
2016-10-23 17:57:24 -0700

    Initial Commit

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class LeagueInvaders {
+GamePanel panel;
+	JFrame frame;
+	int width = 500;
+	int height = 800;
+public static void main(String[] args) {
+	  new LeagueInvaders();
+	
+	
+	
+}
+LeagueInvaders(){
+	panel = new GamePanel();
+	 frame = new JFrame();
+	 setup();
+	
+}
+void setup(){
+	frame.add(panel);
+    frame.addKeyListener(panel);
+	frame.setVisible(true);
+	frame.setSize(width, height);
+	
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+panel.startGame();
+
+}
+}
